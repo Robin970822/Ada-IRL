6ff09a1b2d7094c4a5999022256e82015282a2b4
Robin970822 <717740827@qq.com>
2018-04-25 11:30:17 +0800

    add Sarsa Lambda

--- rl.py
+++ rl.py
@@ -70,7 +70,7 @@ class QLearning(RL):
         self.check_state_exist(s_)
         q_predict = self.q_table.loc[s, a]
         if s_ != 'terminal':
-            q_target = r + self.Gamma * self.q_table.loc[s_, a]
+            q_target = r + self.Gamma * self.q_table.loc[s_, :].max()
         else:
             q_target = r
         self.q_table.loc[s, a] += self.LR * (q_target - q_predict)
@@ -97,3 +97,53 @@ class Sarsa(RL):
         self.q_table.loc[s, a] += self.LR * (q_target - q_predict)
 
 
+# backward eligibility traces
+class SarsaLambda(RL):
+    def __init__(self, actions,
+                 learning_rate=0.01,
+                 reward_decay=0.9,
+                 e_greedy=0.9,
+                 trace_decay=0.9):
+        super(SarsaLambda, self).__init__(actions,
+                                          learning_rate,
+                                          reward_decay,
+                                          e_greedy)
+        self.Lambda = trace_decay
+        self.eligibility_trace = self.q_table
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            to_be_appended = pd.Series(
+                np.zeros(self.n_actions),
+                index=self.q_table.columns,
+                name=state
+            )
+            self.q_table = self.q_table.append(to_be_appended)
+
+            # also update eligibility trace
+            self.eligibility_trace = self.eligibility_trace.append(to_be_appended)
+
+    def learn(self, s, a, r, s_, a_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+        if s_ != 'terminal':
+            q_target = r + self.Gamma * self.q_table.loc[s_, a_]
+        else:
+            q_target = r
+        error = q_target - q_predict
+
+        # increase trace amount for visited state-action pair
+
+        # Method 1:
+        # self.eligibility_trace.loc[s, a] += 1
+
+        # Method 2:
+        self.eligibility_trace.loc[s, :] *= 0
+        self.eligibility_trace.loc[s, a] = 1
+
+        # Q update
+        self.q_table += self.LR * error * self.eligibility_trace
+
+        # decay eligibility trace after update
+        self.eligibility_trace *= self.Gamma * self.Lambda
--- test.py
+++ test.py
@@ -7,8 +7,8 @@ from maze_env import Maze
 from rl import *
 
 
-def update():
-    for episode in range(100):
+def q_update():
+    for episode in range(10):
         print "Episode %d" %(episode)
         # initial observation
         observation = env.reset()
@@ -34,11 +34,49 @@ def update():
                 break
     print 'Game Over'
     print RL.q_table
-    # env.destroy()
+    env.destroy()
+
+
+def sarsa_update():
+    for episode in range(10):
+        print "Episode %d" %(episode)
+        # initial observation
+        observation = env.reset()
+        action = RL.choose_action(str(observation))
+
+        while True:
+            # fresh env
+            env.render()
+
+            # choose action
+            action_ = RL.choose_action(str(observation))
+
+            # make action
+            observation_, reward, done = env.step(action)
+
+            # learn
+            RL.learn(str(observation), action, reward,
+                     str(observation_), action_)
+
+            # update observation
+            observation = observation_
+            action = action_
+
+            if done:
+                break
+    print 'Game Over'
+    print RL.q_table
+    env.destroy()
 
 
 if __name__ == '__main__':
     env = Maze()
     RL = QLearning(actions=list(range(env.n_actions)))
-    env.after(100, update)
+    env.after(100, q_update)
     env.mainloop()
+
+    env = Maze()
+    RL = Sarsa(actions=list(range(env.n_actions)))
+    env.after(100, sarsa_update)
+    env.mainloop()
+
